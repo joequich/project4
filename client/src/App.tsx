c9f71cd8aeb1413e30c43dc11a57d712b4a5d36c
Joseph Quispe <josepheqch@gmail.com>
2021-07-07 22:37:04 -0500

    modified login styles

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,7 @@
 import { FormEvent } from "react";
 import { useForm } from "./hooks/useForm";
+import { User as UserIcon, Mail as MailIcon } from 'react-feather';
+import logo from './assets/logo.svg';
 
 interface FormValues {
     username?: string;
@@ -23,21 +25,34 @@ const App = () => {
         <div className="login-container">
             <div className="login-wrapper">
                 <form onSubmit={handleSubmit}>
-                    <h1>LOG IN</h1>
+                    <div className="logo">
+                        <img src={logo} alt="" style={{height: '48px'}}/>
+                    </div>
+                    <h1 className="headling">LOG IN</h1>
                     <br />
-                    <div className="input-wrapper mb-md">
-                        <input type="text" name="username" className="form-control" placeholder="Username" aria-placeholder="Your user name" autoComplete="off"
-                            value={username}
-                            onChange={handleInputChange}
-                        />
+                    <div className="input-wrapper mb-sm">
+                        <div className="input-container">
+                            <div className="input-icon">
+                                <UserIcon color="black" size={20} />
+                            </div>
+                            <input type="text" name="username" className="input-field" placeholder="Username" aria-placeholder="Your user name" autoComplete="off"
+                                value={username}
+                                onChange={handleInputChange}
+                            />
+                        </div>
                     </div>
-                    <div className="input-wrapper mb-md">
-                        <input type="password" name="password" className="form-control" placeholder="Password" aria-placeholder="Your password" autoComplete="off"
-                            value={password}
-                            onChange={handleInputChange}
-                        />
+                    <div className="input-wrapper mb-sm">
+                        <div className="input-container">
+                            <div className="input-icon">
+                                <MailIcon color="black" size={20} />
+                            </div>
+                            <input type="password" name="password" className="input-field" placeholder="Password" aria-placeholder="Your password" autoComplete="off"
+                                value={password}
+                                onChange={handleInputChange}
+                            />
+                        </div>
                     </div>
-                    <button type="submit" className="button button-login">Save</button>
+                    <button type="submit" className="button button-login">ENTER</button>
                 </form>
             </div>
         </div>
