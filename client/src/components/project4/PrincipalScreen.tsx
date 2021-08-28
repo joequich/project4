73444e99a1ad5e549ef88f4e577f369d57a01fdc
Joseph Quispe <josepheqch@gmail.com>
2021-08-27 23:55:29 -0500

    fixed some styles

--- client/src/components/project4/PrincipalScreen.tsx
+++ client/src/components/project4/PrincipalScreen.tsx
@@ -1,54 +1,178 @@
 import React from 'react';
-import imagen from '../../assets/camera_logitech.jpg';
+import imagen from '../../assets/keyboard_keychron.jpg';
 
 export const PrincipalScreen = () => {
     return (
-            <div className="container">
-                <div className="items-list">
-                    <div className="item-card">
-                        <figure className="item-card__picture">
-                            <img src={imagen} alt="camera" />
-                        </figure>
+            <div className="items-list">
+                <div className="item-card">
+                    <figure className="item-card__figure">
+                        <img src={imagen} alt="camera" />
+                    </figure>
+                    <div className="item-card__content">
                         <h2 className="item-card__title">Title</h2>
-                        <div className="item-card__body">Body</div>
+                        <div className="item-card__star-rating">Stars</div>
+                        <div className="item-card__price">Price</div>
                     </div>
-                    <div className="item-card">
-                        <figure className="item-card__picture">Image</figure>
+                </div>
+                <div className="item-card">
+                    <figure className="item-card__figure">
+                        <img src={imagen} alt="camera" />
+                    </figure>
+                    <div className="item-card__content">
                         <h2 className="item-card__title">Title</h2>
-                        <div className="item-card__body">Body</div>
+                        <div className="item-card__star-rating">Stars</div>
+                        <div className="item-card__price">Price</div>
                     </div>
-                    <div className="item-card">
-                        <figure className="item-card__picture">Image</figure>
+                </div>
+                <div className="item-card">
+                    <figure className="item-card__figure">
+                        <img src={imagen} alt="camera" />
+                    </figure>
+                    <div className="item-card__content">
                         <h2 className="item-card__title">Title</h2>
-                        <div className="item-card__body">Body</div>
+                        <div className="item-card__star-rating">Stars</div>
+                        <div className="item-card__price">Price</div>
                     </div>
-                    <div className="item-card">
-                        <figure className="item-card__picture">Image</figure>
+                </div>
+                <div className="item-card">
+                    <figure className="item-card__figure">
+                        <img src={imagen} alt="camera" />
+                    </figure>
+                    <div className="item-card__content">
                         <h2 className="item-card__title">Title</h2>
-                        <div className="item-card__body">Body</div>
+                        <div className="item-card__star-rating">Stars</div>
+                        <div className="item-card__price">Price</div>
                     </div>
-                    <div className="item-card">
-                        <figure className="item-card__picture">Image</figure>
+                </div>
+                <div className="item-card">
+                    <figure className="item-card__figure">
+                        <img src={imagen} alt="camera" />
+                    </figure>
+                    <div className="item-card__content">
                         <h2 className="item-card__title">Title</h2>
-                        <div className="item-card__body">Body</div>
+                        <div className="item-card__star-rating">Stars</div>
+                        <div className="item-card__price">Price</div>
                     </div>
-                    <div className="item-card">
-                        <figure className="item-card__picture">Image</figure>
+                </div>
+                <div className="item-card">
+                    <figure className="item-card__figure">
+                        <img src={imagen} alt="camera" />
+                    </figure>
+                    <div className="item-card__content">
                         <h2 className="item-card__title">Title</h2>
-                        <div className="item-card__body">Body</div>
+                        <div className="item-card__star-rating">Stars</div>
+                        <div className="item-card__price">Price</div>
                     </div>
-                    <div className="item-card">
-                        <figure className="item-card__picture">Image</figure>
+                </div>
+                <div className="item-card">
+                    <figure className="item-card__figure">
+                        <img src={imagen} alt="camera" />
+                    </figure>
+                    <div className="item-card__content">
                         <h2 className="item-card__title">Title</h2>
-                        <div className="item-card__body">Body</div>
+                        <div className="item-card__star-rating">Stars</div>
+                        <div className="item-card__price">Price</div>
                     </div>
-                    <div className="item-card">
-                        <figure className="item-card__picture">Image</figure>
+                </div>
+                <div className="item-card">
+                    <figure className="item-card__figure">
+                        <img src={imagen} alt="camera" />
+                    </figure>
+                    <div className="item-card__content">
                         <h2 className="item-card__title">Title</h2>
-                        <div className="item-card__body">Body</div>
+                        <div className="item-card__star-rating">Stars</div>
+                        <div className="item-card__price">Price</div>
                     </div>
-                     
-                    
+                </div>
+                <div className="item-card">
+                    <figure className="item-card__figure">
+                        <img src={imagen} alt="camera" />
+                    </figure>
+                    <div className="item-card__content">
+                        <h2 className="item-card__title">Title</h2>
+                        <div className="item-card__star-rating">Stars</div>
+                        <div className="item-card__price">Price</div>
+                    </div>
+                </div>
+                <div className="item-card">
+                    <figure className="item-card__figure">
+                        <img src={imagen} alt="camera" />
+                    </figure>
+                    <div className="item-card__content">
+                        <h2 className="item-card__title">Title</h2>
+                        <div className="item-card__star-rating">Stars</div>
+                        <div className="item-card__price">Price</div>
+                    </div>
+                </div>
+                <div className="item-card">
+                    <figure className="item-card__figure">
+                        <img src={imagen} alt="camera" />
+                    </figure>
+                    <div className="item-card__content">
+                        <h2 className="item-card__title">Title</h2>
+                        <div className="item-card__star-rating">Stars</div>
+                        <div className="item-card__price">Price</div>
+                    </div>
+                </div>
+                <div className="item-card">
+                    <figure className="item-card__figure">
+                        <img src={imagen} alt="camera" />
+                    </figure>
+                    <div className="item-card__content">
+                        <h2 className="item-card__title">Title</h2>
+                        <div className="item-card__star-rating">Stars</div>
+                        <div className="item-card__price">Price</div>
+                    </div>
+                </div>
+                <div className="item-card">
+                    <figure className="item-card__figure">
+                        <img src={imagen} alt="camera" />
+                    </figure>
+                    <div className="item-card__content">
+                        <h2 className="item-card__title">Title</h2>
+                        <div className="item-card__star-rating">Stars</div>
+                        <div className="item-card__price">Price</div>
+                    </div>
+                </div>
+                <div className="item-card">
+                    <figure className="item-card__figure">
+                        <img src={imagen} alt="camera" />
+                    </figure>
+                    <div className="item-card__content">
+                        <h2 className="item-card__title">Title</h2>
+                        <div className="item-card__star-rating">Stars</div>
+                        <div className="item-card__price">Price</div>
+                    </div>
+                </div>
+                <div className="item-card">
+                    <figure className="item-card__figure">
+                        <img src={imagen} alt="camera" />
+                    </figure>
+                    <div className="item-card__content">
+                        <h2 className="item-card__title">Title</h2>
+                        <div className="item-card__star-rating">Stars</div>
+                        <div className="item-card__price">Price</div>
+                    </div>
+                </div>
+                <div className="item-card">
+                    <figure className="item-card__figure">
+                        <img src={imagen} alt="camera" />
+                    </figure>
+                    <div className="item-card__content">
+                        <h2 className="item-card__title">Title</h2>
+                        <div className="item-card__star-rating">Stars</div>
+                        <div className="item-card__price">Price</div>
+                    </div>
+                </div>
+                <div className="item-card">
+                    <figure className="item-card__figure">
+                        <img src={imagen} alt="camera" />
+                    </figure>
+                    <div className="item-card__content">
+                        <h2 className="item-card__title">Title</h2>
+                        <div className="item-card__star-rating">Stars</div>
+                        <div className="item-card__price">Price</div>
+                    </div>
                 </div>
             </div>
     )
