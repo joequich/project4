573862d0922a2e802e724300b528dbe11846473e
Joseph Quispe <joequich@gmail.com>
2022-02-26 18:47:04 -0500

    feat: change jwt middleware to refresh token

--- server/src/auth/middlewares/jwt.middleware.ts
+++ server/src/auth/middlewares/jwt.middleware.ts
@@ -5,6 +5,7 @@ import env from '../../common/config/env.config';
 import { IJwt } from '../../interfaces/jwt.interface';
 
 const jwtAccessSecret = env.JWT.ACCESS_SECRET_KEY || '';
+const jwtRefreshSecret = env.JWT.REFRESH_SECRET_KEY || '';
 
 class JwtMiddleware {
     constructor(private readonly usersService: IUsersService) {}
@@ -36,53 +37,29 @@ class JwtMiddleware {
     }
 
     validateRefreshToken = async ( req: Request, res: Response, next: NextFunction ) => {
-        const authHeader = req.headers.authorization;
-        const authParts = authHeader?.split(' ');
-
-        if (authParts?.length !== 2) {
-            return res.status(401).json({
-                message: 'No authorization token was found in request'
+        if(!req.cookies.Refresh) {
+            return res.status(401).send({
+                message: 'Don\'t exits refresh token'
             });
         }
-
-        if(!/^Bearer$/i.test(authParts[0])) {
-            return res.status(401).json({
-                message: 'Bad format, authorization format is: Bearer [token]'
-            });
-        }
-
-        // it could be a decode step to verify the algorithm type
 
         try {
-            res.locals.jwt = jwt.verify( authParts[1], jwtAccessSecret ) as IJwt;
+            res.locals.jwt = jwt.verify( req.cookies.Refresh, jwtRefreshSecret) as IJwt;
+            const user = await this.usersService.getUserCredentialsByEmail( res.locals.jwt.email);
+            
+            if(!user){
+                return res.status(401).json({ message: 'Invalid Refresh Token' });
+            }
+
+            req.body = {
+                userId: user._id,
+                email: user.email,
+                role: user.role,
+            };
             return next();
         } catch (error) {
-            console.log('Refresh Token exist, but there is an error ', error)
-            console.log('Cookie with refresh token ', req.cookies.Refresh)
             return res.status(401).json({ message: 'Invalid Token' });
         }
-
-        // const user = await this.usersService.getUserCredentialsByEmail(
-        //     res.locals.jwt.email
-        // );
-        // const salt = res.locals.jwt.refreshKey;
-        // const hash = hashSync(res.locals.jwt.userId + jwtSecretKey, salt);
-
-        // if (hash === req.body.refreshToken && user) {
-        //     req.body = {
-        //         userId: user._id,
-        //         email: user.email,
-        //         role: user.role,
-        //     };
-        //     return next();
-        // } else {
-        //     return res
-        //         .status(400)
-        //         .json({ 
-        //             status: 400,
-        //             message: 'Invalid refresh token',
-        //         });
-        // }
     };
 }
 
