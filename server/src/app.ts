94bb8f6a395d333b66e0a863397657e911edebbe
Joseph Quispe <joequich@gmail.com>
2022-03-17 00:53:05 -0500

    fix: change cors methods

--- server/src/app.ts
+++ server/src/app.ts
@@ -12,7 +12,7 @@ if (env.NODE_ENV === 'development') {
 }
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', req.headers.origin);
-    // res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
     res.header("Access-Control-Allow-Headers", "Origin, Authorization, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Credentials", "true");
     next();
